7b1c3f207e877f8b0f6912045fba5b5150b71449
Evangelista <MoisesEvangelista@dpss.lacounty.gov>
2020-04-04 19:08:03 -0700

    added map with rates of cases

--- code.R
+++ code.R
@@ -11,6 +11,8 @@ library(rvest)
 library(tidyverse)
 library(sf)
 
+options(scipen = 999)
+
 rm(list = ls()) #start with empty workspace
 
 # create a folder to download files if it does not exist
@@ -44,7 +46,7 @@ if(file.exists(  paste0(filename,
   dataAsOf <- x %>% 
     html_nodes(".tytul,table") %>% 
     html_text() %>% 
-    sub(".*(\\d{2}?\\D{2} \\d{2}/\\d{2}).*", "\\1", .) %>% 
+    sub(".* (\\d+\\D{2} \\d+/\\d+).*", "\\1", .) %>% 
     paste0(., "/2020") %>% 
     as.POSIXct(., format = '%I%p %m/%d/%Y') %>% 
     .[!is.na(.)]
@@ -54,14 +56,17 @@ if(file.exists(  paste0(filename,
     html_table() %>% 
     .[[1]] %>% 
     as.data.frame() %>% 
+    setNames(.[1,]%>% unlist() %>% as.character()) %>% 
     setNames(gsub("[^[:alnum:]]", "", perl = TRUE, names(.))) %>%
     mutate(type = case_when(grepl("Locations|Deaths|Age Group|^Investigation|CITY/COMMUNITY", Locations ) ~ Locations ),
-           type = sub(".*(Locations|Deaths|Age Group|Investigation|CITY/COMMUNITY).*", "\\1",type),
+           type = sub(".*(Locations|Deaths|Age Group|Investigation|CITY/COMMUNITY).*", "\\1", type),
            type = zoo::na.locf(type,  na.rm = FALSE),
            type = case_when(is.na(type)~ "locations" , 
                             TRUE ~ type),
            TotalCases = gsub("[^[:digit:]]", "", TotalCases),
            TotalCases = as.numeric(TotalCases),
+           Rate = gsub("[^[:digit:]\\.]", "", Rate),
+           Rate = as.numeric(Rate),
            cleanLocations = gsub("[^[:alnum:]/ -]", "", perl = TRUE, Locations),
            cleanLocations = trimws(cleanLocations),
            cleanLocations = gsub("\\s+" , " ", cleanLocations),
@@ -134,7 +139,7 @@ if(file.exists("tigerroads.Rdata")) {
   
   tigerroads = sf::st_read("./shapefiles/tigerroads.shp") %>% 
     filter(grepl("S1200|S1100", MTFCC)) %>% 
-    st_transform(., 2229) %>% # t2000 <- tigerroads %>% # change coordinte system to LA County official system
+    st_transform(., 2229) %>% # t2000 <- tigerroads %>% # change coordinate system to LA County official system
     rmapshaper::ms_simplify(., keep = 0.01,
                             keep_shapes = TRUE) %>% 
     sf::st_transform(., 4326) # change it to GPS satellite navigation system
@@ -145,7 +150,7 @@ if(file.exists("tigerroads.Rdata")) {
   
   unlink(td)
   unlink(tf)
-  unlink(shapefiles)
+  unlink("shapefiles")
   
 }
 
@@ -162,7 +167,7 @@ Communities %>%
 # has values that are not in the shape file or the shape file has duplicates e.g. Covina
 
 clnCnts <- cnts %>%
-  filter(type == "CITY/COMMUNITY" | type == "locations" & grepl("Beach|Pasadena", Locations)
+  filter(type == "CITY/COMMUNITY" | type == "Locations" & grepl("Beach|Pasadena", Locations)
   ) %>% 
   filter(!is.na(TotalCases) & TotalCases > 0) %>%
   mutate(COMMTYPE = case_when(grepl("^Los Angeles", cleanLocations) ~ "Los Angeles",
@@ -238,6 +243,50 @@ labelsBreaks <- pretty(combo_Points %>%
                          filter(!is.na(lon)) %>% 
                          .$TotalCases , 5)
 
+labelsBreaksRate <- pretty(combo_Points %>% 
+                             st_drop_geometry(.) %>% 
+                             filter(!is.na(lon)) %>% 
+                             .$Rate , 5)
+
+labelsBreaksRate <- combo %>%
+  st_drop_geometry() %>% 
+  .$Rate %>% 
+  # .[.<200] %>% #order()
+  BAMMtools::getJenksBreaks(., k = 6, subset = NULL)
+
+age.cat <- function(x, lower = 0, upper, #by = 10,
+                    sep = "-", above.char = "+") {
+  
+  c(paste(seq(lower, upper - by, by = by),
+          seq(lower + by - 1, upper - 1, by = by),
+          sep = sep),
+    paste(upper, above.char, sep = ""))
+  
+}
+
+
+age.cat <- function(x, lower = 0, upper, by = 10,
+                    sep = "-", above.char = "+") {
+  
+  labs <- c(paste(seq(lower, upper - by, by = by),
+                  seq(lower + by - 1, upper - 1, by = by),
+                  sep = sep),
+            paste(upper, above.char, sep = ""))
+  
+  cut(floor(x), breaks = BAMMtools::getJenksBreaks(x, k = 6, subset = NULL),
+      right = FALSE, labels = labs)
+}
+
+
+
+# %>% 
+#   .[, HC04_EST_VC25_Grps := cut(HC04_EST_VC25,
+#                                 # breaks = jenksBreaks$brks, 
+#                                 breaks = BAMMtools::getJenksBreaks(.$HC04_EST_VC25, 6, subset = NULL),
+#                                 include.lowest = TRUE)] %>% 
+#   setattr(., "comment", listOfAttr) # %>%   setattr(., "comment2", listOfAttr2) 
+
+
 matchedCommunities <- combo %>%
   st_drop_geometry(.) %>% 
   group_by(matchType) %>% 
@@ -245,7 +294,7 @@ matchedCommunities <- combo %>%
             NumberOfCommunities = n()) %>% # View
   ungroup
 
-Cairo::CairoPDF(file = "LACountyCOVID19_20200328PM.pdf",
+Cairo::CairoPDF(file = "LACountyCOVID19_2020040412PM.pdf",
                 #units = "in", dpi = 150,
                 width = 8, 
                 height = 11, 
@@ -375,6 +424,74 @@ scale_color_gradient2(
        Sources: http://publichealth.lacounty.gov/media/Coronavirus/locations.htm (counts),
         http://egis3.lacounty.gov/dataportal/wp-content/uploads/2010/10/Communities1.zip (shapefile)")
 
+ggplot() +
+  geom_sf(data = combo
+          , size = .001) +
+  geom_sf(data = combo %>% 
+            filter(!is.na(comm_id)) %>% 
+            mutate(Rate_Groups = cut(Rate,
+                                     # breaks = jenksBreaks$brks, 
+                                     breaks = BAMMtools::getJenksBreaks(.$Rate, 6, subset = NULL),
+                                     include.lowest = TRUE)
+                   # ,Rate_Groups = case_when(grepl(labelsBreaksRate[length(labelsBreaksRate)-1] %>%
+                   #                                 round(.) %>%
+                   #                                 paste0("\\(",.), Rate_Groups) ~ 
+                   #                           paste0(labelsBreaksRate[length(labelsBreaksRate)-1]
+                   #                                  ,"+"), TRUE ~ as.character(Rate_Groups))
+            )
+          , size = .001
+          ,aes(fill = Rate_Groups)) +
+  geom_sf(data = Communities %>% 
+            filter(grepl("Long Beach|^Pasadena", NAME)) %>% 
+            group_by(NAME) %>% 
+            arrange(desc(AREA_SQMI)) %>% 
+            slice(1) %>% 
+            ungroup,
+          fill = NA,
+          color = "red", size = .001) +
+  geom_sf(data = tigerroads %>% 
+            filter(MTFCC == "S1100")
+          ,color = "cornsilk",
+          alpha = .75, size = .1) +
+  scale_fill_brewer(name = "Rate"
+                    ,palette = "Paired"
+                    ,na.value = "grey50") +
+  # scale_size_manual( "Total Cases") +
+  hrbrthemes::theme_ipsum_rc(
+    plot_title_size = 20,
+    subtitle_size = 15,
+    caption_size = 10
+  ) +
+  theme(
+    panel.border = element_blank(),
+    panel.grid.major = element_line(color = "gray91"),
+    panel.grid.minor = element_line(color = "gray94"),
+    strip.background = element_rect(colour = NA,
+                                    fill = "blue"),
+    # element_blank(),
+    strip.text.x = element_text(
+      size = 10,
+      colour = "white",
+      angle = 00,
+      hjust = 0 #, lineheight = .5
+    ),axis.text.x = element_blank()
+    ,axis.text.y = element_blank()
+    ,axis.title = element_blank(),
+    legend.position = "top",
+    legend.box = "vertical",
+    #legend.margin = margin(),
+    legend.key.width = unit(2, "cm")
+  ) +
+  labs(size = "Rate of cases",
+       title = paste0("Novel Coronavirus in Los Angeles County
+                      as of ",
+                      cnts$dataAsOf %>% unique() %>% format("%B %d, %Y (%p)")
+       )
+       ,caption = "Note: Pasadena and Long Beach have their own health departments (show outlined in red)
+       separate from the County.
+       Sources: http://publichealth.lacounty.gov/media/Coronavirus/locations.htm (counts),
+       http://egis3.lacounty.gov/dataportal/wp-content/uploads/2010/10/Communities1.zip (shapefile)")
+
 dev.off()
 
-system(paste0('open "', 'LACountyCOVID19_20200328PM.pdf', '"'))
+system(paste0('open "', 'LACountyCOVID19_2020040412PM.pdf', '"'))
